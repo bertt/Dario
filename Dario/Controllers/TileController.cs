bee9c1c717cf567b8b78dec97a0d17149ea76d72
bertt <btemme@hotmail.com>
2014-02-17 20:29:57 +0100

    tilecontroller

---
+++ Dario/Controllers/TileController.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dario.Controllers
+{
+    public class TileController : ApiController
+    {
+        public HttpResponseMessage GetTile()
+        {
+            var bitmap = new Bitmap(50, 50, PixelFormat.Format32bppArgb);
+            var graphics = Graphics.FromImage(bitmap);
+            var pen = new Pen(Color.Black);
+            graphics.DrawLine(pen,0,0,50,50);
+            return Request.CreateResponse(HttpStatusCode.OK, (Image)bitmap);
+        }
+
+        [Route("{layers}/{level:int:min(0)}/{col:int:min(0)}/{row:int:min(0)}.{ext}")]
+        public HttpResponseMessage GetTile(string layers, string level, int col, int row)
+        {
+            Image image = null;
+            //var image = Tilestacker.Stack(layers, level, col, row);
+            return image != null ?
+                Request.CreateResponse(HttpStatusCode.OK, image) :
+                Request.CreateResponse(HttpStatusCode.NotFound, "No images found");
+        }
+
+    }
+}
---
+++ Dario/Formatters/PngMediaTypeFormatter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Dario.Formatters
+{
+    public class PngMediaTypeFormatter : MediaTypeFormatter
+    {
+        public PngMediaTypeFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("image/png"));
+            MediaTypeMappings.Add(new UriPathExtensionMapping("png", "image/png"));
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, System.Net.Http.HttpContent content, TransportContext transportContext)
+        {
+            var taskSource = new TaskCompletionSource<object>();
+            try
+            {
+                var image = (Image)value;
+                var ms = new MemoryStream();
+                image.Save(ms, ImageFormat.Png);
+                var bytes = ms.ToArray();
+                writeStream.Write(bytes, 0, bytes.Length);
+                taskSource.SetResult(null);
+            }
+            catch (Exception e)
+            {
+                taskSource.SetException(e);
+            }
+            return taskSource.Task;
+        }
+
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return (type == typeof(Image));
+        }
+    }
+}
