1897d91201fea5aec1ad14fcc9df5d1182355645
bertt <btemme@hotmail.com>
2014-02-18 08:33:31 +0100

    added console

---
+++ Dario.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Hosting;
+using System.Threading;
+
+namespace Dario.Console
+{
+    public class Program
+    {
+        private static readonly ManualResetEvent QuitEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            var server = "http://*:{0}";
+            var port = 5001;
+            if (args.Length > 0)
+            {
+                int.TryParse(args[0], out port);
+            }
+            
+            #if DEBUG
+                server = "http://localhost:{0}";
+            #endif
+
+            System.Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                QuitEvent.Set();
+                eArgs.Cancel = true;
+            };
+            using (WebApp.Start<Startup>(string.Format(server, port)))
+            {
+                System.Console.WriteLine("Started");
+                QuitEvent.WaitOne();
+            }
+
+        }
+    }
+}
