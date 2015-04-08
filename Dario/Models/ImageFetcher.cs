e10587c51b3bf8fe545bda2dd97c48558a7b2b9f
pauldendulk <pauldendulk@gmail.com>
2015-04-08 22:11:37 +0200

    Adding configurable web tile sources

---
+++ Dario/Models/ImageFetcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dario.Models
+{
+    class ImageFetcher
+    {
+        readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<Image> Fetch(int col, int row, string level, string urlTemplate)
+        {
+            var url = (string)urlTemplate.Clone();
+            url = url.Replace("{x}", col.ToString(CultureInfo.InvariantCulture));
+            url = url.Replace("{y}", row.ToString(CultureInfo.InvariantCulture));
+            url = url.Replace("{z}", level);
+            return Image.FromStream(await _httpClient.GetStreamAsync(url).ConfigureAwait(false));
+        }
+    }
+}
